4449831d429092f939feab1c9c4683435636886f
Brian Reber <reber.brian@gmail.com>
2012-03-14 22:40:39 -0500

    Add CprE315X Project 2 base files

---
+++ CprE315X/Project2/src/spell-check.cpp
@@ -0,0 +1,12 @@
+/** 
+ * Brian Reber (breber@iastate.edu)
+ * CprE 315X, Spring 2012
+ * Spell Check Programming Assignment
+ */
+#include <iostream>
+#include <float.h>
+#include "spell-check.h"
+
+int main(int argc, char ** argv) {
+
+}
---
+++ CprE315X/Project2/src/spell-check.h
@@ -0,0 +1,10 @@
+/** 
+ * Brian Reber (breber@iastate.edu)
+ * CprE 315X, Spring 2012
+ * Spell Check Programming Assignment
+ */
+#ifndef SPELLCHECK_H
+#define SPELLCHECK_H
+
+
+#endif
