e26e8f80b443b8e2dd074c806ecb20dfc4d45b33
Brian Reber <reber.brian@gmail.com>
2012-04-07 10:07:21 -0500

    Find path appears to work properly

--- CprE315X/Project3/src/match.cpp
+++ CprE315X/Project3/src/match.cpp
@@ -6,12 +6,15 @@
 #include <iostream>
 #include "match.h"
 
+#define DEBUG
+
 using namespace std;
 
 int main(int argc, char ** argv) {
 	fstream inputfile;
 	
-	vector< vector<edge *> * > * nodes = new vector< vector<edge *> * >;;
+	vector< vector<edge *> * > * nodes = new vector< vector<edge *> * >;
+	vector<int> * flows = new vector<int>;
 	
 	int numGuys = 0;
 	int numGals = 0;
@@ -31,10 +34,32 @@ int main(int argc, char ** argv) {
 	inputfile >> numGuys;
 	inputfile >> numGals;
 
+	// Parse the bulk of the data
 	parseData(nodes, numGuys, numGals, inputfile);
 
 	cout << "Num Guys: " << numGuys << endl;
 	cout << "Num Gals: " << numGals << endl;
+
+
+#ifdef DEBUG
+	// Print out the edges for each node
+	for (int i = 0; i < nodes->size(); i++) {
+		cout << "Node " << i << endl;
+		vector<edge *> * cur = nodes->at(i);
+		for (int j = 0; j < cur->size(); j++) {
+			cout << "\t" << cur->at(j)->fromNode << " --> " << cur->at(j)->toNode << " with " << cur->at(j)->flow << " / " << cur->at(j)->capacity << endl;
+		}
+	}
+#endif	
+
+	vector<pathnode *> path;
+	findPath(nodes, 0, nodes->size() - 1, path);
+	
+	cout << "found path" << endl;
+	
+	for (int i = 0; i < path.size(); i++) {
+		cout << i << ": " << path[i]->theEdge->fromNode << " --> " << path[i]->theEdge->toNode << " with " << path[i]->theEdge->flow << " / " << path[i]->theEdge->capacity << endl;
+	}
 
 	return 0;
 }
@@ -55,12 +80,13 @@ void parseData(vector< vector<edge *> * > * nodes, int numGuys, int numGals, fst
 	nodes->push_back(sink);
 
 	// Add the edges from the source to the guys
-	for (int i = 0; i < numGuys; i++) {
+	for (int i = 1; i <= numGuys; i++) {
 		// Add the edge from source to the guy
 		edge * forward = new edge;
 		forward->fromNode = 0;
 		forward->toNode = i;
 		forward->capacity = 1;
+		forward->flow = 0;
 		nodes->at(0)->push_back(forward);
 		
 		// Add the reverse edge
@@ -68,16 +94,18 @@ void parseData(vector< vector<edge *> * > * nodes, int numGuys, int numGals, fst
 		reverse->fromNode = i;
 		reverse->toNode = 0;
 		reverse->capacity = 0;
+		reverse->flow = 0;
 		nodes->at(i)->push_back(reverse);
 	}
-	
+
 	// Add the edges from the gals to the sink
-	for (int i = 0; i < numGals; i++) {
+	for (int i = numGuys + 1; i <= numGuys + numGals; i++) {
 		// Add the edge from gal to the sink
 		edge * forward = new edge;
 		forward->fromNode = i;
 		forward->toNode = nodes->size() - 1;
 		forward->capacity = 1;
+		forward->flow = 0;
 		nodes->at(i)->push_back(forward);
 		
 		// Add the reverse edge
@@ -85,6 +113,7 @@ void parseData(vector< vector<edge *> * > * nodes, int numGuys, int numGals, fst
 		reverse->fromNode = nodes->size() - 1;
 		reverse->toNode = i;
 		reverse->capacity = 0;
+		reverse->flow = 0;
 		nodes->at(nodes->size() - 1)->push_back(reverse);
 	}
 
@@ -95,11 +124,16 @@ void parseData(vector< vector<edge *> * > * nodes, int numGuys, int numGals, fst
 		inputfile >> guy;
 		inputfile >> gal;
 		
+		// So each node has a unique id, gals will be given the id
+		// numGuys + gal
+		gal += numGuys;
+		
 		// Add the edge from guy to gal
 		edge * forward = new edge;
 		forward->fromNode = guy;
 		forward->toNode = gal;
 		forward->capacity = INT_MAX;
+		forward->flow = 0;
 		nodes->at(guy)->push_back(forward);
 		
 		// Add the reverse edge
@@ -107,6 +141,79 @@ void parseData(vector< vector<edge *> * > * nodes, int numGuys, int numGals, fst
 		reverse->fromNode = guy;
 		reverse->toNode = gal;
 		reverse->capacity = 0;
+		reverse->flow = 0;
 		nodes->at(gal)->push_back(reverse);
 	}
+}
+
+
+
+vector<pathnode *> * findPath(vector< vector<edge *> * > * nodes, int fromNode, 
+								int toNode, vector<pathnode *> & path) {
+	cout << "findPath(" << fromNode << ", " << toNode << ", " << path.size() << ");" << endl;
+								
+	// We are currently at our destination, so return the path
+	// we have built up to this point
+	if (fromNode == toNode) {
+		return &path;
+	}
+	
+	// Find all of the edges coming out of our source node, and
+	// iterate through them, trying to find a forward edge that
+	// still has capacity remaining
+	vector<edge *> * sourceEdges = nodes->at(fromNode);
+	for (int i = 0; i < sourceEdges->size(); i++) {
+		// Get the current edge to look at, and
+		// find the residual value (since we don't store it)
+		edge * cur = sourceEdges->at(i);
+		int residualValue = cur->capacity - cur->flow;
+		
+		// If there is still space for us to flow forward,
+		// continue investigating this path
+		if (residualValue > 0) {
+			bool contains = false;
+			pathnode * temp = new pathnode;
+			temp->value = residualValue;
+			temp->theEdge = cur;
+			
+			cout << cur->fromNode << " --> " << cur->toNode << ": " << residualValue << " / " << cur->capacity << endl;
+			
+			for (int j = 0; j < path.size(); j++) {
+				if ((path[j]->value == residualValue) && compareEdges(path[j]->theEdge, cur)) {
+					contains = true;
+					break;
+				}
+			}
+			
+			cout << "Contains? " << contains << endl;
+			
+			vector<pathnode *> * currentPath;
+			// If we don't already have this edge in our path, add it and find a path from
+			// the current node to the toNode
+			if (!contains) {
+				path.push_back(temp);
+				currentPath = findPath(nodes, cur->toNode, toNode, path);
+			}
+			
+			if (currentPath != NULL) {
+				// We recursively found a path from this node to another
+				// so return that path
+				return currentPath;
+			} else {
+				// We didn't find a path, so remove the previously added node
+				// so we can continue looking
+				path.pop_back();
+			}
+		}
+	}
+	
+	return NULL;
+}
+
+bool compareEdges(edge * edge1, edge * edge2) {
+	bool from = (edge1->fromNode == edge2->fromNode);
+	bool to = (edge1->toNode == edge2->toNode);
+	bool cap = (edge1->capacity == edge2->capacity);
+	
+	return from && to && cap;
 }
--- CprE315X/Project3/src/match.h
+++ CprE315X/Project3/src/match.h
@@ -13,8 +13,18 @@ typedef struct {
 	int fromNode;
 	int toNode;
 	int capacity;
+	int flow;
 } edge;
 
+typedef struct {
+	int value;
+	edge * theEdge;
+} pathnode;
+
 void parseData(std::vector< std::vector<edge *> * > *, int, int, std::fstream &);
+
+std::vector<pathnode *> * findPath(std::vector< std::vector<edge *> * > *, int, int, std::vector<pathnode *> &);
+
+bool compareEdges(edge *, edge *);
 
 #endif
