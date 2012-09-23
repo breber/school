0a0bf5898a8ef3a21ca4f503237568ae399e566c
Brian Reber <reber.brian@gmail.com>
2012-09-23 17:15:47 -0500

    Add a lot of functionality to XText

--- SE319/HW3/src/XTextbreber.java
+++ SE319/HW3/src/XTextbreber.java
@@ -1,12 +1,19 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.filechooser.FileFilter;
 
 /**
  * XText - A Text Editor
@@ -17,6 +24,11 @@ import javax.swing.JTextArea;
  */
 public class XTextbreber extends JFrame {
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The JTextArea we use
+	 */
+	private JTextArea mTextArea = new JTextArea();
 
 	/**
 	 * Main entry point
@@ -34,12 +46,13 @@ public class XTextbreber extends JFrame {
 		setSize(800, 600);
 
 		// Build the text area
-		JTextArea textArea = new JTextArea();
-		textArea.setEditable(true);
-		textArea.setLineWrap(true);
-		//		textArea.setBackground(Color.GREEN);
+		mTextArea.setEditable(true);
+		mTextArea.setLineWrap(true);
 
-		getContentPane().add(new JScrollPane(textArea));
+		// Perform actions necessary to create a new document
+		newDocument();
+
+		getContentPane().add(new JScrollPane(mTextArea));
 	}
 
 	private JMenuBar buildMenuBar() {
@@ -49,17 +62,51 @@ public class XTextbreber extends JFrame {
 		// Setup the File Menu
 		JMenu fileMenu = new JMenu("File");
 
+		// New Menu Option
 		JMenuItem newMenuItem = new JMenuItem("New");
 		newMenuItem.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
+				saveIfFileModified();
+
+				newDocument();
 			}
 		});
 		fileMenu.add(newMenuItem);
-		fileMenu.add(new JMenuItem("Open"));
-		fileMenu.add(new JMenuItem("Save"));
-		fileMenu.add(new JMenuItem("Exit"));
+
+		// Open Menu Item
+		JMenuItem openMenuItem = new JMenuItem("Open");
+		openMenuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				saveIfFileModified();
+
+				openDocument();
+			}
+		});
+		fileMenu.add(openMenuItem);
+
+		// Save Menu Item
+		JMenuItem saveMenuItem = new JMenuItem("Save");
+		saveMenuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				saveDocument();
+			}
+		});
+		fileMenu.add(saveMenuItem);
+
+		// Exit Menu Item
+		JMenuItem exitMenuItem = new JMenuItem("Exit");
+		exitMenuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				saveIfFileModified();
+
+				System.exit(0);
+			}
+		});
+		fileMenu.add(exitMenuItem);
 		menuBar.add(fileMenu);
 
 		// Setup the Edit Menu
@@ -77,6 +124,112 @@ public class XTextbreber extends JFrame {
 		menuBar.add(editMenu);
 
 		return menuBar;
+	}
+
+	/**
+	 * Create a new Document
+	 * 
+	 * -Sets the title
+	 * -Clears the text area
+	 */
+	public void newDocument() {
+		// Update the window title
+		setTitle("XText:New");
+
+		// Clear the text area
+		mTextArea.setText("");
+	}
+
+	/**
+	 * Check to see if the current file needs saving, prompt the user
+	 * to see if they want to save it, and then save it if they do.
+	 */
+	private void saveIfFileModified() {
+		// TODO: save old?
+		if (true) {
+			// If the document content has changed, prompt the user to see if they
+			// want to save their work
+			int saveResult = JOptionPane.showConfirmDialog(XTextbreber.this, "Would you like to save your work?", "", JOptionPane.YES_NO_OPTION);
+
+			// If the user responded yes, perform the save operation
+			if (JOptionPane.YES_OPTION == saveResult) {
+				saveDocument();
+			}
+		}
+	}
+
+	/**
+	 * Perform a save operation by prompting the user to choose where to
+	 * save the document
+	 */
+	public void saveDocument() {
+		JFileChooser saveFileChooser = new JFileChooser();
+		saveFileChooser.setDialogType(JFileChooser.SAVE_DIALOG);
+		saveFileChooser.setFileFilter(new FileFilter() {
+			@Override
+			public String getDescription() {
+				return "Text documents";
+			}
+
+			@Override
+			public boolean accept(File arg0) {
+				return arg0.getName().contains(".text") || arg0.getName().contains(".txt");
+			}
+		});
+
+		int saveResult = saveFileChooser.showSaveDialog(this);
+
+		if (JFileChooser.APPROVE_OPTION == saveResult) {
+			File f = saveFileChooser.getSelectedFile();
+			FileOutputStream fos;
+			try {
+				fos = new FileOutputStream(f);
+				fos.write(mTextArea.getText().getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Prompts the user to open a file, then replaces the text in the JTextArea
+	 * with the contents of that file
+	 */
+	public void openDocument() {
+		JFileChooser openFileChooser = new JFileChooser();
+		openFileChooser.setDialogType(JFileChooser.OPEN_DIALOG);
+		openFileChooser.setFileFilter(new FileFilter() {
+			@Override
+			public String getDescription() {
+				return "Text documents";
+			}
+
+			@Override
+			public boolean accept(File arg0) {
+				return arg0.getName().contains(".text") || arg0.getName().contains(".txt");
+			}
+		});
+
+		int saveResult = openFileChooser.showOpenDialog(this);
+
+		if (JFileChooser.APPROVE_OPTION == saveResult) {
+			File f = openFileChooser.getSelectedFile();
+
+			setTitle("XText:" + f.getName());
+
+			try {
+				Scanner scanner = new Scanner(f);
+				StringBuilder temp = new StringBuilder();
+				while (scanner.hasNextLine()) {
+					temp.append(scanner.nextLine());
+					temp.append("\n");
+				}
+
+				mTextArea.setText(temp.toString());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
