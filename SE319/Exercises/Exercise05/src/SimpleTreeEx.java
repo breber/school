1a4a30eb3d25971fa2264267570cfdb5ceda84c4
Brian Reber <reber.brian@gmail.com>
2012-10-05 12:58:54 -0500

    Build the JTree properly

--- SE319/Exercises/Exercise05/src/SimpleTreeEx.java
+++ SE319/Exercises/Exercise05/src/SimpleTreeEx.java
@@ -1,49 +1,49 @@
 import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
 
 
 /**
- * 
+ *
  * @author sbasu
  *   Main window of the UI
  *   - mainFrame of type JFrame contains ErdosStructurePanel of type JPanel
  */
 public class SimpleTreeEx extends JFrame
-{	
+{
 	public static void main(String[] args)
 	{
 		/**
 		 * erdosStruct contains a tree where node of the tree represents a computer scientist
 		 * - there is an edge from one node to another if the computer scientists associated
-		 *   to these nodes have co-authored a scientific article 
+		 *   to these nodes have co-authored a scientific article
 		 *   (See createStruct method in ErdosStruct class for details)
 		 */
 		ErdosStruct erdosStruct = new ErdosStruct();
 		erdosStruct.createStruct();
-		
+
 		SimpleTreeEx mainFrame = new SimpleTreeEx();
 		/**
 		 * ErdosStructPanel constructor takes a parameter of type ErdosStruct
 		 */
 		mainFrame.getContentPane().add(new ErdosStructPanel(erdosStruct));
-		
+
 		mainFrame.setSize(500, 500);
 		mainFrame.setVisible(true);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+
 }
 
 class ErdosStructPanel extends JPanel
 {
-	
+
 	/**
-	 * 
+	 *
 	 * @param erdosStruct contains the tree data structure with information on co-authorship relations
 	 * You are required to implement this constructor such that the tree data from erdosStruct is used
 	 * to create a corresponding Swing tree UI component
@@ -52,6 +52,27 @@ class ErdosStructPanel extends JPanel
 	 */
 	public ErdosStructPanel(ErdosStruct erdosStruct)
 	{
+		AuthNode node = erdosStruct.getRoot();
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode(node.toString());
 
-	}	
+		buildTree(root, node);
+
+		JTree tree = new JTree(root);
+		tree.setRootVisible(false);
+		tree.setShowsRootHandles(true);
+
+		JScrollPane sp = new JScrollPane(tree);
+		sp.setPreferredSize(new Dimension(450, 450));
+		add(sp);
+	}
+
+	private void buildTree(DefaultMutableTreeNode tree, AuthNode node) {
+		DefaultMutableTreeNode current = new DefaultMutableTreeNode(node.toString());
+
+		for (int i = 0; i < node.getCoAuthCount(); i++) {
+			buildTree(current, node.getCoAuth(i));
+		}
+
+		tree.add(current);
+	}
 }
