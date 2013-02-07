16ac6521bb3f475b3350063d44f6e12ae43ddfcc
Brian Reber <reber.brian@gmail.com>
2013-02-06 21:37:14 -0600

    Work on Experiment1

--- CprE419X/Lab03/src/Experiment1.java
+++ CprE419X/Lab03/src/Experiment1.java
@@ -5,6 +5,10 @@
   */
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
@@ -33,146 +37,51 @@ public class Experiment1 extends Configured implements Tool {
 	
 	public int run ( String[] args ) throws Exception {
 		
-		String input = "/datasets/Lab3/Graph";
+//		String input = "/datasets/Lab3/Graph";
+		String input = "/user/breber/ex1.txt";
 		String temp = "/user/breber/Lab3/temp";
 		String output = "/user/breber/Lab3/output/";
 		
 		int reduce_tasks = 2;  // The number of reduce tasks that will be assigned to the job
 		Configuration conf = new Configuration();
 		
-		// Create job for round 1
-		
 		// Create the job
 		Job job_one = new Job(conf, Experiment1.class.getName() + " Round One"); 
-		
-		// Attach the job to this Driver
 		job_one.setJarByClass(Experiment1.class); 
-		
-		// Fix the number of reduce tasks to run
-		// If not provided, the system decides on its own
 		job_one.setNumReduceTasks(reduce_tasks);
-		
-		// The datatype of the Output Key 
-		// Must match with the declaration of the Reducer Class
+
 		job_one.setOutputKeyClass(Text.class); 
-		
-		// The datatype of the Output Value 
-		// Must match with the declaration of the Reducer Class
 		job_one.setOutputValueClass(Text.class);
 		
-		// The class that provides the map method
 		job_one.setMapperClass(Map_One.class); 
-		
-		// The class that provides the reduce method
 		job_one.setReducerClass(Reduce_One.class);
 		
-		// Decides how the input will be split
-		// We are using TextInputFormat which splits the data line by line
-		// This means wach map method recieves one line as an input
 		job_one.setInputFormatClass(TextInputFormat.class);  
-		
-		// Decides the Output Format
 		job_one.setOutputFormatClass(TextOutputFormat.class);
 		
-		// The input HDFS path for this job
-		// The path can be a directory containing several files
-		// You can add multiple input paths including multiple directories
 		FileInputFormat.addInputPath(job_one, new Path(input)); 
-		
-		// The output HDFS path for this job
-		// The output path must be one and only one
-		// This must not be shared with other running jobs in the system
 		FileOutputFormat.setOutputPath(job_one, new Path(temp + "round1"));
 		
-		// Run the job
 		job_one.waitForCompletion(true); 
 		
 		
-		// Create job for round 2
-		// The output of the previous job can be passed as the input to the next
-		// The steps are as in job 1
 		Job job_two = new Job(conf, Experiment1.class.getName() + " Round Two"); 
 		job_two.setJarByClass(Experiment1.class); 
-		job_two.setNumReduceTasks(reduce_tasks); 
-		
+		job_two.setNumReduceTasks(reduce_tasks);
+
 		job_two.setOutputKeyClass(Text.class); 
 		job_two.setOutputValueClass(Text.class);
 		
-		// If required the same Map / Reduce classes can also be used
-		// Will depend on logic if separate Map / Reduce classes are needed
-		// Here we show separate ones
 		job_two.setMapperClass(Map_Two.class); 
 		job_two.setReducerClass(Reduce_Two.class);
 		
-		job_two.setInputFormatClass(TextInputFormat.class); 
+		job_two.setInputFormatClass(TextInputFormat.class);  
 		job_two.setOutputFormatClass(TextOutputFormat.class);
 		
-		// The output of previous job set as input of the next
 		FileInputFormat.addInputPath(job_two, new Path(temp + "round1")); 
 		FileOutputFormat.setOutputPath(job_two, new Path(temp + "round2"));
 		
-		// Run the job
-		job_two.waitForCompletion(true);
-		
-		
-		// Create job for round 3
-		// The output of the previous job can be passed as the input to the next
-		// The steps are as in job 1
-
-		Job job_three = new Job(conf, Experiment1.class.getName() + " Round Three"); 
-		job_three.setJarByClass(Experiment1.class); 
-		job_three.setNumReduceTasks(reduce_tasks); 
-
-		job_three.setOutputKeyClass(Text.class); 
-		job_three.setOutputValueClass(IntWritable.class);
-
-		// If required the same Map / Reduce classes can also be used
-		// Will depend on logic if separate Map / Reduce classes are needed
-		// Here we show separate ones
-		job_three.setMapperClass(Map_Three.class); 
-		job_three.setReducerClass(Reduce_Three.class);
-
-		job_three.setInputFormatClass(TextInputFormat.class); 
-		job_three.setOutputFormatClass(TextOutputFormat.class);
-
-		// The output of previous job set as input of the next
-		FileInputFormat.addInputPath(job_three, new Path(temp + "round1"));
-		FileInputFormat.addInputPath(job_three, new Path(temp + "round2"));
-		
-		FileOutputFormat.setOutputPath(job_three, new Path(temp + "round3"));
-
-		// Run the job
-		job_three.waitForCompletion(true); 
-		
-		
-		// Create job for round 4
-		// The output of the previous job can be passed as the input to the next
-		// The steps are as in job 1
-
-		Job job_four = new Job(conf, Experiment1.class.getName() + " Round Four"); 
-		job_four.setJarByClass(Experiment1.class); 
-		job_four.setNumReduceTasks(1); 
-
-		job_four.setOutputKeyClass(IntWritable.class); 
-		job_four.setOutputValueClass(Text.class);
-		job_four.setSortComparatorClass(IntComparator.class);
-
-		// If required the same Map / Reduce classes can also be used
-		// Will depend on logic if separate Map / Reduce classes are needed
-		// Here we show separate ones
-		job_four.setMapperClass(Map_Four.class); 
-		job_four.setReducerClass(Reduce_Four.class);
-
-		job_four.setInputFormatClass(TextInputFormat.class); 
-		job_four.setOutputFormatClass(TextOutputFormat.class);
-
-		// The output of previous job set as input of the next
-		FileInputFormat.addInputPath(job_four, new Path(temp + "round3"));
-		
-		FileOutputFormat.setOutputPath(job_four, new Path(output));
-
-		// Run the job
-		job_four.waitForCompletion(true); 
+		job_two.waitForCompletion(true); 
 		
 		return 0;
 	} // End run
@@ -200,28 +109,8 @@ public class Experiment1 extends Configured implements Tool {
 		}
 	}
 	
-	// The Map Class
-	// The input to the map method would be a LongWritable (long) key and Text (String) value
-	// Notice the class declaration is done with LongWritable key and Text value
-	// The TextInputFormat splits the data line by line.
-	// The key for TextInputFormat is nothing but the line number and hence can be ignored
-	// The value for the TextInputFormat is a line of text from the input
-	// The map method can emit data using context.write() method
-	// However, to match the class declaration, it must emit Text as key and IntWribale as value
 	public static class Map_One extends Mapper<LongWritable, Text, Text, Text>  {
-		/**
-		 * @param key - the line nunmber
-		 * @param value - a line in format of "citing_patent cited_patent" 
-		 * 
-		 * Outputs:
-		 * 
-		 * cited_patent citing_patent
-		 */
-		public void map(LongWritable key, Text value, Context context) 
-								throws IOException, InterruptedException  {
-			
-			// The TextInputFormat splits the data line by line.
-			// So each map method receives one line from the input
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String line = value.toString();
 			
 			// Tokenize to get the two vertices
@@ -232,163 +121,84 @@ public class Experiment1 extends Configured implements Tool {
 			
 			// Output the cited patent, with a value of the citing patent
 			context.write(new Text(vertex2), new Text(vertex1));
-		} // End method "map"
-	} // End Class Map_One
+			context.write(new Text(vertex1), new Text("(" + vertex2.toString() + "," + vertex1.toString() + ")"));
+		}
+	}
 	
-	
-	// The reduce class
-	// The key is Text and must match the datatype of the output key of the map method
-	// The value is IntWritable and also must match the datatype of the output value of the map method
 	public static class Reduce_One extends Reducer<Text, Text, Text, Text>  {
-		/**
-		 * @param key - a patent node
-		 * @param values - all nodes that cite the key patent
-		 * 
-		 * Output: All one-hop citations
-		 * 
-		 * cited_patent  list_of_citing_patents
-		 */
-		public void reduce(Text key, Iterable<Text> values, Context context) 
-											throws IOException, InterruptedException  {
-			
-			StringBuilder output = new StringBuilder(); 
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			List<String> prev = new ArrayList<String>();
 			for (Text val : values) {
-				output.append(val.toString() + " ");
+				String valString = val.toString();
+				if (valString.matches("\\(.+?,.+?\\)")) {
+					context.write(val, new Text(""));
+					
+					for (String s : prev) {
+						String temp = valString.substring(0, valString.length() - 1);
+						temp = temp + "," + s + ")";
+						
+						context.write(new Text(temp), new Text(""));
+					}
+				} else {
+					prev.add(valString);
+					
+					context.write(new Text("(" + key.toString() + "," + valString + ")"), new Text(""));
+				}
 			}
-
-			context.write(key, new Text(output.toString()));
-		} // End method "reduce" 
-	} // End Class Reduce_One
+		}
+	}
 	
-	// The second Map Class
+	
 	public static class Map_Two extends Mapper<LongWritable, Text, Text, Text>  {
-		/**
-		 * @param key - the line nunmber
-		 * @param value - a line in format of "cited_patent [TAB] citing_patent citing_patent citing_patent ..."
-		 * 
-		 * Output:
-		 * 
-		 * citing_patent cited_patent
-		 */
- 		public void map(LongWritable key, Text value, Context context) 
-				throws IOException, InterruptedException  {
-			String line = value.toString();
-			StringTokenizer tokens = new StringTokenizer(line);
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException  {
+			String line = value.toString().substring(1, value.toString().length() - 1);
 			
-			String citedPatent = tokens.nextToken();
+			System.out.println("Map Val: " + value.toString());
+			
+			// Tokenize to get the two vertices
+			StringTokenizer tokens = new StringTokenizer(line, ",");
+			
+			String vertex1 = tokens.nextToken();
+			StringBuilder stringBuilder = new StringBuilder("(");
 			
 			while (tokens.hasMoreTokens()) {
-				context.write(new Text(tokens.nextToken()), new Text(citedPatent));
+				stringBuilder.append(tokens.nextToken());
+
+				if (tokens.hasMoreTokens()) {
+					stringBuilder.append(",");
+				}
 			}
-		}  // End method "map"
-	}  // End Class Map_Two
+			
+			stringBuilder.append(")");
+			
+			// Output the cited patent, with a value of the citing patent
+			context.write(new Text(vertex1), new Text(stringBuilder.toString()));
+		}
+	}
 	
-	// The second Reduce class
 	public static class Reduce_Two extends Reducer<Text, Text, Text, Text>  {
-		/**
-		 * @param key - a patent node
-		 * @param values - all nodes that cite the key patent (two hop)
-		 * 
-		 * Output:  All two-hop citations
-		 * 
-		 * cited_patent  list_of_citing_patents
-		 */
-		public void reduce(Text key, Iterable<Text> values, Context context) 
-				throws IOException, InterruptedException  {
-
-			StringBuilder output = new StringBuilder(); 
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			Map<String, String> set = new HashMap<String, String>();
 			for (Text val : values) {
-				output.append(val.toString() + " ");
+				System.out.println("Received Key: " + key.toString() + " Val: " + val.toString());
+				String valString = val.toString();
+				valString = valString.replace("(", "");
+				valString = valString.replace(")", "");
+				
+				if (valString.contains(",")) {
+					set.put(valString.substring(0, valString.indexOf(",")), valString);
+				} else {
+					set.put(valString, valString);
+				}
 			}
-
-			context.write(key, new Text(output.toString()));
-		}  // End method "reduce"
-	}  // End Class Reduce_Two
+			
+			System.out.println("Set Key: " + set.keySet() + " Vals: " + set.values());
+			
+			// TODO: could probably be simplified
+			for (String s : set.values()) {
+				context.write(key, new Text(s));
+			}
+		}
+	}
 	
-	
-	// The second Map Class
-		public static class Map_Three extends Mapper<LongWritable, Text, Text, IntWritable>  {
-			/**
-			 * @param key - the line nunmber
-			 * @param value - a line in format of "cited_patent [TAB] citing_patent citing_patent citing_patent ..."
-			 * 
-			 * Output:
-			 * 
-			 * cited_patent number_of_citing_patents
-			 */
-			public void map(LongWritable key, Text value, Context context) 
-					throws IOException, InterruptedException  {
-				String line = value.toString();
-				StringTokenizer tokens = new StringTokenizer(line);
-				
-				String citedPatent = tokens.nextToken();
-				
-				int count = 0;
-				while (tokens.hasMoreTokens()) {
-					tokens.nextToken();
-					count++;
-				}
-				context.write(new Text(citedPatent), new IntWritable(count));
-			}  // End method "map"
-		}  // End Class Map_Two
-
-		// The second Reduce class
-		public static class Reduce_Three extends Reducer<Text, IntWritable, Text, IntWritable>  {
-			/**
-			 * @param key - a patent node
-			 * @param values - the number of citing patents
-			 * 
-			 * Output:  The total number of citing patents
-			 * 
-			 * cited_patent number_of_citing_patents
-			 */
-			public void reduce(Text key, Iterable<IntWritable> values, Context context) 
-					throws IOException, InterruptedException  {
-
-				int sum = 0;
-				
-				for (IntWritable val : values) {
-					sum += val.get();
-				}
-				
-				context.write(key, new IntWritable(sum));
-			}  // End method "reduce"
-		}  // End Class Reduce_Three
-		
-		// The second Map Class
-		public static class Map_Four extends Mapper<LongWritable, Text, IntWritable, Text>  {
-			public void map(LongWritable key, Text value, Context context) 
-					throws IOException, InterruptedException  {
-				String line = value.toString();
-
-				// Tokenize to get the individual words
-				StringTokenizer tokens = new StringTokenizer(line);
-				
-				String citedPatent = tokens.nextToken();
-				String count = tokens.nextToken();
-				context.write(new IntWritable(Integer.parseInt(count)), new Text(citedPatent));
-			}  // End method "map"
-		}  // End Class Map_Two
-
-		// The second Reduce class
-		public static class Reduce_Four extends Reducer<IntWritable, Text, Text, IntWritable>  {
-
-			private static int count = 0;
-
-			public void reduce(IntWritable key, Iterable<Text> values, Context context) 
-					throws IOException, InterruptedException  {
-
-				// Print out all values that have the higest key value
-				// Stop printing when we get to 10 values.
-				// This solution only works since we specify 1 reducer.
-				for (Text val : values) {
-					if (count < 10) {
-						context.write(val, key);
-						count++;
-					} else {
-						break;
-					}
-				}
-			}  // End method "reduce"
-		}  // End Class Reduce_Two
 }
