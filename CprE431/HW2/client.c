5707ad20bbbf5350169e20a9a042a8c644cbf84e
Brian Reber <reber.brian@gmail.com>
2013-02-15 20:49:52 -0600

    Print out the message

--- CprE431/HW2/client.c
+++ CprE431/HW2/client.c
@@ -11,11 +11,14 @@
 
 #define TRUE 1
 #define ITERATION_SIZE 5
-#define THRESHOLD 100
+#define THRESHOLD 35000000
 
 int main(int argc, char ** argv) {
 	char messageReceived[1000];
+	int i = 0;
 	
+	memset(messageReceived, 0, sizeof(messageReceived));
+
 	while (TRUE) {
 		int count = 0;
 		time_t startTime = time(NULL) % 86400;
@@ -24,5 +27,12 @@ int main(int argc, char ** argv) {
 		}
 		
 		printf("count = %d\n", count);
+		if (count < THRESHOLD) {
+			messageReceived[i++] = '1';
+		} else {
+			messageReceived[i++] = '0';
+		}
+
+		printf("Message: %s\n", messageReceived);
 	}
 }
