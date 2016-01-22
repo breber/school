cc95b5be6072b494ffcf456bf56656e6683ac5fc
Brian Reber <reber.brian@gmail.com>
2016-01-21 20:12:23 -0600

    Implement problem 2

--- CprE535/HW1/DisplayVignette.m
+++ CprE535/HW1/DisplayVignette.m
@@ -1,9 +1,15 @@
 % This code is for Homework 1, Vignetting, in Math 535, spring 2016
 % and was authored by Brian Reber
+clear();
+close();
 
 image = 'images/image3.jpg';
+itemsPerRow = 3;
 scaleFactors = [0, 0.25, 0.30, 0.5, 0.8, 1];
 
+index = 1;
 for factor = scaleFactors
-    imshow(VignettingEffect(image, factor));
+    subplot(size(scaleFactors, 2) / itemsPerRow, itemsPerRow, index), imshow(VignettingEffect(image, factor));
+    title(strcat('Scale factor =  ', num2str(factor)));
+    index = index + 1;
 end
--- CprE535/HW1/VignettingEffect.m
+++ CprE535/HW1/VignettingEffect.m
@@ -2,7 +2,19 @@
 % and was authored by Brian Reber
 
 function [ vignetteImg ] = VignettingEffect(inputImage, scaleLevel)
-    vignetteImg = imread(inputImage);
+    imageInfo = imfinfo(inputImage);
+    inputImageData = imread(inputImage);
     
-    % TODO: impelement vignette
+    centerY = imageInfo.Height / 2;
+    centerX = imageInfo.Width / 2;
+    
+    vignetteImg = zeros(imageInfo.Height, imageInfo.Width, 'uint8');
+    
+    maxRadius = sqrt(centerY ^ 2 + centerX ^ 2);
+    for x = 1:imageInfo.Width
+        for y = 1:imageInfo.Height
+            radius = sqrt((x - centerX) ^ 2 + (y - centerY) ^ 2);
+            vignetteImg(y, x) = inputImageData(y, x) * (1 - scaleLevel * radius / maxRadius);
+        end
+    end
 end
