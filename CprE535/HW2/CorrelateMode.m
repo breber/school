d59c8aaed87c011d66b9545a8cae2de0fa45b8ff
Brian Reber <reber.brian@gmail.com>
2016-03-05 18:15:04 -0600

    Don't include 0 correlation value

--- CprE535/HW2/CorrelateMode.m
+++ CprE535/HW2/CorrelateMode.m
@@ -7,14 +7,20 @@ function [ status ] = CorrelateMode( inPath, inAvg )
     end
 
     nList = size(iList, 2);
+    results = zeros([nList - 1, 1]);
+    index = 1;
     for i = 1:nList
         if 0 == strcmp(inAvg, iList{i})
             fprintf('Correlating: %s --> %s\n', inAvg, iList{i});
             prnuName = {inAvg, iList{i}};
-            [status, ref1, ref2, colrNames, corr] = lucasdigicamident('correlate', inPath, '', prnuName);
-            corr
-            %fprintf('    Results: %f, %f, %f\n', corr{1}, corr{2}, corr{3});
+            [status, ~, ~, ~, corr] = lucasdigicamident('correlate', inPath, '', prnuName);
+            results(index) = corr(1) + corr(2) + corr(3);
+            fprintf('    Results: %f, %f, %f --> %f\n', corr(1), corr(2), corr(3), results(index));
+            
+            index = index + 1;
         end
     end
+    
+    %[0, 0.1, 0.2, 0.3, 0.4, 0.5]
+    histogram(results);
 end
-
