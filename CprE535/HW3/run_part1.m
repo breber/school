b052562f69bfb41d924317cbcdf004674707fbf6
Brian Reber <reber.brian@gmail.com>
2016-03-30 19:32:22 -0500

    Use proper percent

--- CprE535/HW3/run_part1.m
+++ CprE535/HW3/run_part1.m
@@ -19,23 +19,25 @@ percents = [0, 15, 25, 75, 100];
 figure_index = 1;
 for percent = percents
     image = imread(covername);
-    embed_percent_length = uint32(ceil(length(image) * percent / 100));
+    embed_percent_length = uint32(ceil(length(image) * length(image) * percent / 100));
     
     % now embed the message
     for x = 1:embed_percent_length
-        for y = 1:embed_percent_length
-            if use_replacement == 1
-                image(x, y) = bitset(image(x, y), 1, message(x, y));
-            else
-                % LSB matching algorithm:
-                % - if the message bit == cover image bit, do nothing
-                % - if the message bit != cover image bit:
-                %   - pick random value (r) between [-1, 1]
-                %   - stego(x, y) = cover(x, y) + r
-                if message(x, y) ~= bitget(image(x, y), 1)
-                    r = randi([-1 1], 1, 1);
-                    image(x, y) = image(x, y) + r;
-                end
+        % Find the pixel location
+        i = idivide(x, length(image) + 1, 'floor') + 1;
+        j = mod(x - 1, length(image)) + 1;
+        
+        if use_replacement == 1
+            image(i, j) = bitset(image(i, j), 1, message(i, j));
+        else
+            % LSB matching algorithm:
+            % - if the message bit == cover image bit, do nothing
+            % - if the message bit != cover image bit:
+            %   - pick random value (r) between [-1, 1]
+            %   - stego(x, y) = cover(x, y) + r
+            if message(i, j) ~= bitget(image(i, j), 1)
+                r = randi([-1 1], 1, 1);
+                image(i, j) = image(i, j) + r;
             end
         end
     end
@@ -45,20 +47,19 @@ for percent = percents
     figure_index = figure_index + 1;
     
     % For each percentage, calculate the p value
-    p_vals = zeros(101, 3);
-    for factor = 3:101
-        actual_factor = (factor - 1);
-        sublength = uint16(ceil(length(image) * actual_factor / 100));
-        sub_cover = image(1:sublength, 1:sublength);
+    p_vals = zeros(100, 2);
+    for factor = 1:100
+        sublength = uint32(ceil(length(image) * length(image) * factor / 100));
+        reshaped_image = reshape(image, length(image) * length(image), 1, 3);
+        sub_cover = reshaped_image(1:sublength, 1, 1);
         p_vals(factor, 1) = chisq(sub_cover);
-        p_vals(factor, 2) = sublength;
-        p_vals(factor, 3) = actual_factor / 100;
+        p_vals(factor, 2) = factor / 100;
     end
     
     % Write the p-value structures out to a csv
     csvwrite(strcat('pvals_', int2str(percent), '.csv'), p_vals);
 
     % Draw the p-value graph
-    figure(figure_index), plot(p_vals(:, 3), p_vals(:, 1)); title(['P-Values (Embed ', int2str(percent), '%)']);
+    figure(figure_index), plot(p_vals(:, 2), p_vals(:, 1)); title(['P-Values (Embed ', int2str(percent), '%)']);
     figure_index = figure_index + 1;
 end
