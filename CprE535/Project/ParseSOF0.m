acaef1316d9dd3e017aabf802c7d519d1de3fab5
Brian Reber <reber.brian@gmail.com>
2016-04-26 19:56:47 -0500

    Write SOF

--- CprE535/Project/ParseSOF0.m
+++ CprE535/Project/ParseSOF0.m
@@ -1,5 +1,16 @@
 function [ unused ] = ParseSOF0(~)
 %ParseSOF0 Handle the SOF0 marker
-%   No special handling is needed
+%  Length (2 bytes)
+%  Sample Precision (1 byte)
+%  Num Lines (2 bytes)
+%  Samples per line (2 bytes)
+%  Num Components (1 byte)
+%  Component:
+%    ID (1 byte)
+%    Horizontal Sample Factor (4 bits)
+%    Vertical Sample Factor (4 bits)
+%    Quanization Table Selector (1 byte)
+
+    % No special handling is needed
     unused = 0;
 end
--- CprE535/Project/save_jpeg.m
+++ CprE535/Project/save_jpeg.m
@@ -15,9 +15,7 @@ function [] = save_jpeg( output_path, rgb_data, scan_data )
     SaveDHT(file_id, scan_data.DHT);
 
     % Write SOF
-    fwrite(file_id, hex2dec('FF'), 'uint8');
-    fwrite(file_id, hex2dec('C0'), 'uint8');
-    % TODO
+    SaveSOF0(file_id, rgb_data);
 
     % Write Scan Data
     fwrite(file_id, hex2dec('FF'), 'uint8');
