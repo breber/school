55114aa644d3ebdd0c0830a6257780690b749751
Brian Reber <reber.brian@gmail.com>
2016-07-08 20:24:05 -0500

    Update keys

--- CprE632/config.py
+++ CprE632/config.py
@@ -1,18 +1,14 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# TODO: change password
-SQLALCHEMY_DATABASE_URI = 'mysql://root:cdc@localhost/country'
-
+SQLALCHEMY_DATABASE_URI = 'mysql://root:632iowastatepassword@localhost/country'
+# For session management
+SECRET_KEY = '\x83\xf0\x18\x93\x97\x8dx\xf5\xe1\xe4\xf0L\x13A\xac\xa6G\xa4@V\xa3Yy '
 WTF_CSRF_ENABLED = False
-
-# TODO: change secret
-SECRET_KEY = 'It-is-a-secret'
 
 COUNTRY_NAME = 'Duloc'
 
 # UN Settings
-# TODO: use real key
-UN_NATION_KEY = 'test-nation-key'
+UN_NATION_KEY = 'prd5eXc+KWZ/J+uaHRqu3A=='
 UN_URL = 'http://un.hq.net' # NO TRAILING SLASH
 UN_ORIGIN_FQDN = 'mil.du'
--- CprE632/run.py
+++ CprE632/run.py
@@ -3,4 +3,4 @@ import sys
 sys.path.append('/usr/local/lib/python2.7/dist-packages')
 
 from app import app
-app.run(host='0.0.0.0', debug=True)
+app.run(host='0.0.0.0', debug=False)
